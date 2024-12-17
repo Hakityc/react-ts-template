7b02bacef2f4d36f2693cb2e10525ec1c44f85f6
Tang <>
2024-12-17 15:39:24 +0800

    build(dev): 添加 @types/node 并配置路径别名
    - 在 devDependencies 中添加 @types/node 依赖
    - 在 vite.config.ts 中配置路径别名，使 src 目录下的文件可以通过 @ 访问

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { resolve } from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      //设置别名以便src下的文件可以使用@找到
+      "@": resolve(__dirname, "src"),
+    },
+  },
+});
